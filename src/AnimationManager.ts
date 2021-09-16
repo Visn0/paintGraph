525236b862a3b00c7d0649832613dba1d07b1e0f
Carlos Eduardo Arismendi Sanchez <carlos.arismendisanchez@gmail.com>
2021-09-16 18:38:32 +0200

    Cell animations

--- src/AnimationManager.ts
+++ src/AnimationManager.ts
@@ -21,9 +21,10 @@ class AnimationManager {
     this.#animationCounter += 1
     setTimeout(() => {
       this.#animationCounter -= 1
-      let prevColor: string = elem.style.backgroundColor ? elem.style.backgroundColor : "rgba(163, 80, 220, 0.20)"
-      prevColor = prevColor.split(',')[3]
-      elem.style.backgroundColor = `rgba(163, 80, 255, ${parseFloat(prevColor) + 0.04})`
+      // let prevColor: string = elem.style.backgroundColor ? elem.style.backgroundColor : "rgba(163, 80, 220, 0.20)"
+      // prevColor = prevColor.split(',')[3]
+      // elem.style.backgroundColor = `rgba(163, 80, 255, ${parseFloat(prevColor) + 0.04})`
+      elem.className = 'explored-node'
     }, this.#getDelay(animationDelay))
   }
 
@@ -42,27 +43,27 @@ class AnimationManager {
       switch (type)
       {
         case CellType.BEGIN:
-          elem.className = 'bg-primary'
+          elem.className = 'begin-node'
           elem.style.backgroundColor = ''
           break
 
         case CellType.WALL:
-          elem.className = 'bg-secondary'
+          elem.className = 'wall-node'
           elem.style.backgroundColor = ''
           break
 
         case CellType.EXIT:
-          elem.className = 'bg-danger'
+          elem.className = 'exit-node'
           elem.style.backgroundColor = ''
           break
 
         case CellType.EMPTY:
-          elem.className = ''
+          elem.className = 'empty-node'
           elem.style.backgroundColor = ''
           break
 
         case CellType.PATH:
-          elem.className = 'bg-success'
+          elem.className = 'path-node'
           elem.style.backgroundColor = ''
           break
       }
